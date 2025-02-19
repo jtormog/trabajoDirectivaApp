3020ba92f88def023570e7d50d284d026418d282
Josep Tormo García <joseptormogarcia@gmail.com>
2025-02-19 19:10:59 +0100

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.spec.ts 	modified:   src/app/app.component.ts 	new file:   src/app/empleado.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'trabajoDirectivaApp' title`, () => {
+  it(`should have the correct title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('trabajoDirectivaApp');
+    expect(app.titulo).toEqual('Listado de Empleados');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, trabajoDirectivaApp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Listado de Empleados');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Empleado } from './empleado';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Listado de Empleados';
+
+  empleados:Empleado[] = [];
+
+  constructor(){
+    this.empleados = [
+      {
+        nombre: 'Juan',
+        apellido: 'Diaz',
+        cargo: 'presidente',
+        salario: 7500
+      },
+      {
+        nombre: 'Ana',
+        apellido: 'Martín',
+        cargo: 'directora',
+        salario: 7500
+      },
+      {
+        nombre: 'Juan',
+        apellido: 'Diaz',
+        cargo: 'jefa de sección',
+        salario: 7500
+      },
+      {
+        nombre: 'Juan',
+        apellido: 'Diaz',
+        cargo: 'administrativo',
+        salario: 7500
+      },
+    ];
+  }
+
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+  }
+
+  cuadroNombre:string = '';
+  cuadroApellido:string = '';
+  cuadroCargo:string = '';
+  cuadroSalario:number = 0;
 }
